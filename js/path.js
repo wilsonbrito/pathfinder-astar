fc4aae2b8efc4bb31a2b50c03f155eb0bf1d2024
wilsonbrito <wilsonjb93@gmail.com>
2018-10-05 21:50:44 -0300

    Ajuste no JS

--- js/path.js
+++ js/path.js
@@ -175,21 +175,21 @@ g.addNode('rua1-3', {
 	holes: 0,
 	semaphore: 0,
 	x: 1,
-	y: 6
+	y: 8
 });
 
 g.addNode('rua1-2', {
 	holes: 0,
 	semaphore: 0,
 	x: 1,
-	y: 8
+	y: 10
 });
 
 g.addNode('rua1-1', {
 	holes: 0,
 	semaphore: 0,
 	x: 1,
-	y: 10
+	y: 12
 });
 
 g.addNode('rua11-5', {
@@ -426,35 +426,35 @@ g.addNode('rua3-com-rua4-1', {
 g.addNode('rua4-5', {
 	holes: 0,
 	semaphore: 0,
-	x: 6,
+	x: 5,
 	y: 40
 });
 
 g.addNode('rua4-4', {
 	holes: 0,
 	semaphore: 0,
-	x: 9,
+	x: 8,
 	y: 40
 });
 
 g.addNode('rua4-3', {
 	holes: 0,
 	semaphore: 0,
-	x: 11,
+	x: 10,
 	y: 40
 });
 
 g.addNode('rua4-2', {
 	holes: 0,
 	semaphore: 0,
-	x: 13,
+	x: 12,
 	y: 40
 });
 
 g.addNode('rua4-1', {
 	holes: 0,
 	semaphore: 0,
-	x: 15,
+	x: 14,
 	y: 40
 });
 
@@ -745,11 +745,279 @@ g.addNode('rua13-1', {
 	y: 38
 });
 
-
-
 /*Link das ruas*/
-g.addLink('r1-b1', 'r2-b1-01', {
+g.addLink('rua1-com-rua10-2', 'rua1-com-rua10-1', {
 	x: 1,
+	y: 1
+});
+
+g.addLink('rua1-com-rua10-1', 'rua1-5', {
+	x: 1,
+	y: 3
+});
+
+g.addLink('rua1-5', 'rua1-4', {
+	x: 1,
+	y: 5
+});
+
+g.addLink('rua1-4', 'rua1-3', {
+	x: 1,
+	y: 7
+});
+
+g.addLink('rua1-3', 'rua1-2', {
+	x: 1,
+	y: 9
+});
+
+g.addLink('rua1-2', 'rua1-1', {
+	x: 1,
+	y: 11
+});
+
+g.addLink('rua1-1', 'conexao1216', {
+	x: 1,
+	y: 13
+});
+
+g.addLink('conexao1216', 'rua2-5', {
+	x: 1,
+	y: 15
+});
+
+g.addLink('rua2-5', 'rua2-4', {
+	x: 1,
+	y: 17
+});
+
+g.addLink('rua2-4', 'rua2-3', {
+	x: 1,
+	y: 19
+});
+
+g.addLink('rua2-3', 'rua2-2', {
+	x: 1,
+	y: 21
+});
+
+g.addLink('rua2-2', 'rua2-1', {
+	x: 1,
+	y: 23
+});
+
+g.addLink('rua2-1', 'conexao2314', {
+	x: 1,
+	y: 25
+});
+
+g.addLink('conexao2314', 'rua3-5', {
+	x: 1,
+	y: 27
+});
+
+g.addLink('rua3-5', 'rua3-4', {
+	x: 1,
+	y: 29
+});
+
+g.addLink('rua3-4', 'rua3-3', {
+	x: 1,
+	y: 31
+});
+
+g.addLink('rua3-3', 'rua3-2', {
+	x: 1,
+	y: 33
+});
+
+g.addLink('rua3-2', 'rua3-1', {
+	x: 1,
+	y: 35
+});
+
+g.addLink('rua3-1', 'rua3-com-rua4-3', {
+	x: 1,
+	y: 37
+});
+
+g.addLink('rua3-com-rua4-3', 'rua3-com-rua4-2', {
+	x: 1,
+	y: 39
+});
+
+g.addLink('rua3-com-rua4-2', 'rua3-com-rua4-1', {
+	x: 2,
+	y: 40
+});
+
+g.addLink('rua3-com-rua4-1', 'rua4-5', {
+	x: 4,
+	y: 40
+});
+
+g.addLink('rua4-5', 'rua4-4', {
+	x: 7,
+	y: 40
+});
+
+g.addLink('rua4-4', 'rua4-3', {
+	x: 9,
+	y: 40
+});
+
+g.addLink('rua4-3', 'rua4-2', {
+	x: 11,
+	y: 40
+});
+
+g.addLink('rua4-2', 'rua4-1', {
+	x: 13,
+	y: 40
+});
+
+g.addLink('rua4-1', 'conexao4513', {
+	x: 15,
+	y: 40
+});
+
+g.addLink('conexao4513', 'rua5-5', {
+	x: 17,
+	y: 40
+});
+
+g.addLink('rua5-5', 'rua5-4', {
+	x: 19,
+	y: 40
+});
+
+g.addLink('rua5-4', 'rua5-3', {
+	x: 21,
+	y: 40
+});
+
+g.addLink('rua5-3', 'rua5-2', {
+	x: 23,
+	y: 40
+});
+
+g.addLink('rua5-2', 'rua5-1', {
+	x: 26,
+	y: 40
+});
+
+g.addLink('rua5-1', 'rua5-com-rua6-3', {
+	x: 28,
+	y: 40
+});
+
+g.addLink('rua5-com-rua6-3', 'rua5-com-rua6-2', {
+	x: 30,
+	y: 40
+});
+
+g.addLink('rua5-com-rua6-2', 'rua5-com-rua6-1', {
+	x: 31,
+	y: 39
+});
+
+g.addLink('rua5-com-rua6-1', 'rua6-1', {
+	x: 31,
+	y: 37
+});
+
+g.addLink('rua6-1', 'rua6-2', {
+	x: 31,
+	y: 35
+});
+
+g.addLink('rua6-2', 'rua6-3', {
+	x: 31,
+	y: 33
+});
+
+g.addLink('rua6-3', 'rua6-4', {
+	x: 31,
+	y: 31
+});
+
+g.addLink('rua6-4', 'rua6-5', {
+	x: 31,
+	y: 29
+});
+
+g.addLink('rua6-5', 'conexao67', {
+	x: 31,
+	y: 27
+});
+
+g.addLink('conexao67', 'rua7-1', {
+	x: 31,
+	y: 25
+});
+
+g.addLink('rua7-1', 'rua7-2', {
+	x: 31,
+	y: 23
+});
+
+g.addLink('rua7-2', 'rua7-3', {
+	x: 31,
+	y: 21
+});
+
+g.addLink('rua7-3', 'rua7-4', {
+	x: 31,
+	y: 19
+});
+
+g.addLink('rua7-4', 'rua7-5', {
+	x: 31,
+	y: 17
+});
+
+g.addLink('rua7-4', 'conexao78', {
+	x: 31,
+	y: 15
+});
+
+g.addLink('conexao78', 'rua8-1', {
+	x: 31,
+	y: 13
+});
+
+g.addLink('rua8-1', 'rua8-2', {
+	x: 31,
+	y: 11
+});
+
+g.addLink('rua8-2', 'rua8-3', {
+	x: 31,
+	y: 9
+});
+
+g.addLink('rua8-3', 'rua8-4', {
+	x: 31,
+	y: 7
+});
+
+g.addLink('rua8-4', 'rua8-5', {
+	x: 31,
+	y: 5
+});
+
+g.addLink('rua8-5', 'rua8-com-rua9-3', {
+	x: 31,
+	y: 3
+});
+
+g.addLink('rua8-com-rua9-3', 'rua8-com-rua9-2', {
+	x: 31,
+	y: 1
+});
+
+g.addLink('rua8-com-rua9-2', 'rua8-com-rua9-1', {
+	x: 29,
 	y: 1
 });
 
@@ -784,14 +1052,16 @@ function foundpath() {
 	let from = document.getElementById('from');
 	let to = document.getElementById('to');
 	console.log(from.value)
+	console.log(to.value)
 
 	let foundPath = pathFinder.find(from.value, to.value);
+
 	foundPath.forEach(function (node) {
 		console.log("Node" + node)
 		var nodeSelector = node.id;
-		console.log('.' + nodeSelector + " .ico");
+		console.log('.' + nodeSelector + " .cls-1");
 		//console.log("Resultado"+result);
-		document.querySelector('.' + nodeSelector + " .ico").setAttribute('fill', 'green')
+		document.querySelector('path#' + nodeSelector).setAttribute('fill', 'green')
 	});
 
 }
